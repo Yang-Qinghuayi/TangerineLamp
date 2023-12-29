bfb9808f2ada539673e4edf2655d1173a1e10a30
yqhy <2112562745@qq.com>
2023-12-29 15:37:36 +0800

    收藏的音乐可见

--- miniprogram/pages/index3/collection/collection.js
+++ miniprogram/pages/index3/collection/collection.js
@@ -1,8 +1,8 @@
 // pages/index0/passage/passage.js
-const db=wx.cloud.database()
-const app = getApp()
-const _ = db.command
-let flag=true
+const db = wx.cloud.database();
+const app = getApp();
+const _ = db.command;
+let flag = true;
 let cnt = 0;
 
 Page({
@@ -11,6 +11,8 @@ Page({
    */
   data: {
     collectedArticles: [], //文章详情列表
+    collectedMusics: [], //音乐详情列表
+    collectedMovies: [], //电影详情列表
   },
 
   /**
@@ -46,32 +48,64 @@ Page({
           collectedArticles: res.data,
         });
       });
+
+
+    // 获取个人的音乐收藏数据
+    db.collection("collected_music")
+      .where({
+        _openid: app.globalData.openid,
+      })
+      .limit(8)
+      .get()
+      .then((res) => {
+        this.setData({
+          collectedMusics: res.data,
+        });
+      });
+
+    // 获取个人的电影收藏数据
+    db.collection("collected_movie")
+      .where({
+        _openid: app.globalData.openid,
+      })
+      .limit(8)
+      .get()
+      .then((res) => {
+        this.setData({
+          collectedMovies: res.data,
+        });
+      });
   },
 
   onReachBottom: function () {
     let oldData = this.data.collectedArticles;
-    if(oldData.length<cnt){
+    if (oldData.length < cnt) {
       wx.showLoading({
-        title: '加载中',
-      })
-      db.collection("collected_article ").skip(oldData.length).limit(8).where({
-        _openid: app.globalData.openid,
-      }).get().then(res=>{
-        let newList = res.data;
-        let newData = oldData.concat(newList);
-        this.setData({
-          collectedArticles :newData
+        title: "加载中",
+      });
+      db.collection("collected_article ")
+        .skip(oldData.length)
+        .limit(8)
+        .where({
+          _openid: app.globalData.openid,
         })
-      })
+        .get()
+        .then((res) => {
+          let newList = res.data;
+          let newData = oldData.concat(newList);
+          this.setData({
+            collectedArticles: newData,
+          });
+        });
       wx.hideLoading();
-    }else{
+    } else {
       wx.showToast({
-        title: '到底了哦',
-        icon: 'success',
-        duration: 1000
-      })
+        title: "到底了哦",
+        icon: "success",
+        duration: 1000,
+      });
     }
-  }, 
+  },
 
   //  获得已收藏文章的数目
   getcount() {
