406e3975b1c9d8fa0109c002d045cb0685477cf2
SkyXJW <2661178033@qq.com>
2024-01-23 21:01:20 +0800

    心灵空间页面初步布局

--- miniprogram/pages/index2/index2.js
+++ miniprogram/pages/index2/index2.js
@@ -1,6 +1,12 @@
 // pages/index2/index2.js
 const app = getApp()
+const date = new Date()
+const years = []
+for (let i = 2000; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
 Page({
+  
 
   /**
    * 页面的初始数据
@@ -29,18 +35,23 @@ Page({
       "cloud://tangerine-cloud-9grdz5e80159e7b3.7461-tangerine-cloud-9grdz5e80159e7b3-1304921980/index2/slides/slide3.jpg",
     ],
     tempTreeholeName: null, // 临时存放树洞名称的地方
+    cur_question:"eeeeeeee",
+    cur_date: '',
+    cur_year: '',
+    value: [9999],
+    years
   },
 
   /**
    * 跳转到对应分区
    */
-  gotoTreehole: function(res) {
-    let temp = res.currentTarget.dataset.treeholename
+  gotoTreehole: function(e) {
+    let temp = e.currentTarget.dataset.treeholename
     console.log("即将前往树洞：",temp, this.data.TreeholeAddress[temp])
     // let tempurl = this.data.TreeholeAddress + "?title=" + temp
     let tempurl = "/pages/index2/treeHole/treeHole?title=" + temp 
     wx.navigateTo({
-      url: tempurl,
+      url: tempurl
     })
   },
 
@@ -65,8 +76,26 @@ Page({
       })
     }
   },
+  bindYearPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index_year: e.detail.value
+    })
+  },
+  // 年份选择的响应事件
+  bindChange1: function(e) {
+    const val = e.detail.value
+    this.setData({
+      cur_year: this.data.years[val[0]].toString(),
+    })
+  },
 
   onLoad(){
-    console.log('是否登录',app.globalData.isLogin)
+    let date = new Date();
+    console.log(date.getDate())
+    this.setData({
+      cur_date : date.getDate().toString()
+    })
+    console.log('是否登录',app.globalData.isLogin,date)
   }
 })
