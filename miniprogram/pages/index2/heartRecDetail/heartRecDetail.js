17b24ffb303a60b4e1d3a6e9fee116845f4b72ac
SkyXJW <2661178033@qq.com>
2024-01-30 17:12:01 +0800

    心灵日记图片上传前览

--- miniprogram/pages/index2/heartRecDetail/heartRecDetail.js
+++ miniprogram/pages/index2/heartRecDetail/heartRecDetail.js
@@ -8,7 +8,8 @@ Page({
     cur_year:'',
     cur_month_date:'',
     cur_question:"今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好",
-    cur_answer:"别急,啊...我先来说两句,呐...先说哪两句呢?嗯...这次就先说这两句今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好"
+    cur_answer:"别急,啊...我先来说两句,呐...先说哪两句呢?嗯...这次就先说这两句今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好",
+    imageList: []  // 存储图片的临时路径
   },
 
   /**
@@ -21,11 +22,63 @@ Page({
       cur_month_date: options.date.substring(5).toString()
     })
   },
+  //取消编辑
+  cancel:function(){
+    wx.navigateBack({
+      delta: 1 // 返回的页面数，如果 delta 大于现有页面数，则返回到首页
+    });
+  },
+  //保存编辑
+  save:function(){
+    //上传图片
+    this.uploadImages()
+    //保存文本
+    //
+
+  },
   // 更新 textarea 的值
   bindInput: function(e) {
     console.log("我来")
     this.setData({
       cur_answer: e.detail.value
+    });
+  },
+  // 点击图片上传，进行图片选择
+  selectpic: function() {
+    const that = this;
+    wx.chooseImage({
+      count: 50,  // 允许选择的图片数量
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function(res) {
+        const tempFilePaths = res.tempFilePaths;
+        that.setData({
+          imageList: that.data.imageList.concat(tempFilePaths)
+        });
+      }
+    });
+  },
+  // 删除(预览中)已上传图片
+  deleteImage: function(e) {
+    const index = e.currentTarget.dataset.index;
+    let imageList = this.data.imageList;
+    imageList.splice(index, 1);  // 删除指定索引的图片
+    this.setData({ imageList });
+  },
+  // 上传图片
+  uploadImages: function() {
+    this.data.imageList.forEach((imgPath) => {
+      wx.uploadFile({
+        url: '上传图片的服务器地址',
+        filePath: imgPath,
+        name: 'file',  // 服务器端接收文件的字段名
+        success(res) {
+          // 处理上传成功
+        },
+        fail(err) {
+          // 处理上传失败
+        }
+      });
     });
   },
 
