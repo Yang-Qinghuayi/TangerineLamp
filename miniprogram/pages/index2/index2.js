8c0738f650699c5a6bbd657c20934bf9df33106f
SkyXJW <2661178033@qq.com>
2024-02-08 20:46:35 +0800

    调整了一下心灵空间首页显示数据:

--- miniprogram/pages/index2/index2.js
+++ miniprogram/pages/index2/index2.js
@@ -1,6 +1,8 @@
 // pages/index2/index2.js
 const app = getApp()
 const date = new Date()
+const db = wx.cloud.database()
+const monthList = ["1","2","3","4","5","6","7","8","9","10","11","12"]
 const years = []
 for (let i = 2000; i <= date.getFullYear(); i++) {
   years.push(i)
@@ -35,8 +37,22 @@ Page({
       "cloud://tangerine-cloud-9grdz5e80159e7b3.7461-tangerine-cloud-9grdz5e80159e7b3-1304921980/index2/slides/slide2.jpg",
       "cloud://tangerine-cloud-9grdz5e80159e7b3.7461-tangerine-cloud-9grdz5e80159e7b3-1304921980/index2/slides/slide3.jpg",
     ],
+    commentList:{//每个月的评论
+      "1":"",
+      "2":"",
+      "3":"",
+      "4":"",
+      "5":"",
+      "6":"",
+      "7":"",
+      "8":"",
+      "9":"",
+      "10":"",
+      "11":"",
+      "12":""
+    },
     tempTreeholeName: null, // 临时存放树洞名称的地方
-    cur_question:"今日问题好好好好好好好好啊好好啊好后今日问题好好好好好好好好啊好好啊好今日问题好好好好好好好好啊好好啊好",
+    cur_question:"",
     cur_date: '',
     cur_year: date.getFullYear().toString(),
     value: [9999],
@@ -107,5 +123,73 @@ Page({
       cur_date : date.getDate().toString()
     })
     console.log('是否登录',app.globalData.isLogin,date)
-  }
+  },
+
+  getQuestion:function(){
+    const that = this
+    db.collection('index2HeartRec_question').where({
+      day: that.data.cur_date
+    }).field({
+      question: true
+    }).limit(100) // 限制返回结果数量
+    .get({
+      success: function(res){
+        console.log("问题查询成功", res)
+        that.setData({
+          cur_question: res.data[0].question
+        })
+      },
+      fail: function(err) {
+        console.log('查询失败', err);
+      }
+    });
+  },
+   // 获取评论
+   getCommentPic:function(month){
+    const that = this
+    let openid = app.globalData.openid
+    db.collection('index2HeartRec_commentAndpicture').where({
+      _openid: openid,
+      year: this.data.cur_year,
+      month: month,
+      day: this.data.cur_date
+    }).field({
+      // picID: true,
+      comment: true,
+      isNone: true
+    }).limit(100) // 限制返回结果数量
+    .get({
+      success: function(res) {
+        console.log('查询成功', month,res.data.length);
+       // 获取评论
+       let monthKey = "commentList[" + month + "]"
+      //  let imageKey = "imageList[" + month + "]"
+       if(res.data.length==0){//当未查询到数据时，显示默认评论
+        that.setData({
+        [monthKey]:"等待你来探索..."
+        // [imageKey]: [defaultImg]
+        })
+       }
+       that.setData({//当未查询到数据时，这段代码不会执行
+         [monthKey]: (res.data[0].comment.length==0) ? "等待你来探索...":res.data[0].comment
+       })
+       // 获取图片
+      //  let fileIDs = res.data.map(item => item.picID); // 将 fileID 提取出来
+      //  console.log('fileIDs', fileIDs); // 打印包含所有 picID 的数组
+      //  let imageKey = "imageList[" + month + "]"
+      //  that.setData({//当未查询到数据时，这段代码不会执行
+      //    [imageKey]: fileIDs
+      //  })
+      },
+      fail: function(err) {
+        console.error('查询失败', err);
+      }
+    });
+  },
+  onShow() {
+    this.getQuestion()
+    monthList.forEach((month)=>
+     this.getCommentPic(month)
+    );
+  },
 })
