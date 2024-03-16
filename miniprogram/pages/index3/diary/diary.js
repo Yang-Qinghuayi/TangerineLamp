eb54c407f5a2121d23de640622ffc6cd463b7077
yqhy <2112562745@qq.com>
2024-03-16 17:10:56 +0800

    实现日志的显示

--- miniprogram/pages/index3/diary/diary.js
+++ miniprogram/pages/index3/diary/diary.js
@@ -1,64 +1,59 @@
 // pages/index3/diary/diary.js
-const db = wx.cloud.database()
-const app = getApp()
+const db = wx.cloud.database();
+const app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     diary_list: [],
     openid: "",
-    _id: ""
+    _id: "",
   },
-  // sendNewMood(res){
-  //   var title=res.detail.value.title;
-  //   //var mood=res.detail.value.mood;
-  //   var content=res.detail.value.content;
-  //   var is_hide=res.detail.value.is_hide;
-  //   db.collection("index3_diary").add({
-  //     data:{
-  //       title:title,
-  //       content:content,
-  //       is_hide:is_hide
-  //     }
-  //   }).then(res=>{
-  //     console.log(res)
-  //   })
-  // }
-  /**
-   * 生命周期函数--监听页面加载
-   */
-
-  //跳转页面
-  go_to_edit() {
+  toDetail: function (e) {
+    console.log("即将前往：", id, this.data.diary_list[id]);
+    const id = e.currentTarget.dataset.dateid;
+    console.log("param1", id);
+    let year = this.data.diary_list[id].year;
+    let month = this.data.diary_list[id].month;
+    let day = this.data.diary_list[id].day;
+    console.log("param2", year, month, day);
+    let tempurl =
+      "/pages/index2/heartRecDetail/heartRecDetail?date=" +
+      year +
+      "-" +
+      month +
+      "月" +
+      day +
+      "日";
     wx.navigateTo({
-      url: "/pages/index3/diary/diary_edit/diary"
+      url: tempurl,
     });
-
   },
-  //  数据库获取最新的20条日志(参数可修改), 对应数据库里某一个用户下所有数据
   getData() {
-    db.collection("index3_diary")
+    db.collection("index2HeartRec_commentAndpicture")
       .where({
-        _openid: this.data.openid
+        _openid: this.data.openid,
       })
       .limit(20)
       .get()
-      .then(res => {
+      .then((res) => {
         this.setData({
-          diary_list: res.data
-        })
-      })
+          diary_list: res.data,
+        });
+      });
   },
   onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: "小橘灯",
+    });
     this.setData({
-      openid: app.globalData.openid
-    })
-    console.log(this.data.openid)
+      openid: app.globalData.openid,
+    });
+    console.log(this.data.openid);
     wx.showLoading({
-      title: 'loading'
-    })
+      title: "loading",
+    });
     this.getData();
     wx.hideLoading();
   },
@@ -66,9 +61,7 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
@@ -80,35 +73,25 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
