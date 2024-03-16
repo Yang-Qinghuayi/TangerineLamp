04a2bf4f4721d56cc0a11bb204412a91ccf62a21
yqhy <2112562745@qq.com>
2024-03-16 14:46:53 +0800

    修改了一点样式吧

--- miniprogram/pages/index1/test1/testlists/pcl-5/pcl-5.js
+++ miniprogram/pages/index1/test1/testlists/pcl-5/pcl-5.js
@@ -1,30 +1,34 @@
 // miniprogram/pages/index1/test1/testlists/pcl-5/pcl-5.js
 const db = wx.cloud.database();
-const _ = db.command
+const _ = db.command;
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    totalScores:0,
+    totalScores: 0,
     // totalScores:10,
     totalColor: "",
-    Parts_Scores:[], //各部分分数
+    Parts_Scores: [], //各部分分数
     final_eval_str: "", // 最终测评结果字符串
     // final_eval_str: "有极轻微PSTD症状，可自行观察一段时间，或向心理医师寻求建议。",
     // Parts_Scores:[["入侵症状",1,"症状轻微"],["持续回避",2,"中等症状"],["认知、情绪消极改变",3,"症状较严重"],["情绪激动",4,"症状严重"]],    // *****
-    Parts_numbers:[[1,2,3,4,5], [6,7],[8,9,10,11,12,13,14],[15,16,17,18,19,20]],  //各部分题目序号
-    Parts_meanings: ["入侵症状","持续回避","认知和情绪消极改变","情绪激动"],  //各部分分数含义
-    showCalculation:false,
+    Parts_numbers: [
+      [1, 2, 3, 4, 5],
+      [6, 7],
+      [8, 9, 10, 11, 12, 13, 14],
+      [15, 16, 17, 18, 19, 20],
+    ], //各部分题目序号
+    Parts_meanings: ["入侵症状", "持续回避", "认知和情绪消极改变", "情绪激动"], //各部分分数含义
+    showCalculation: false,
     // showCalculation:true,      // *****
-    currentChosen:-1,
-    nowIndex:1,
-    chosenAnswers:[0], //第0位仅用来占位，真正的index从1开始
-    chosenAnswersLength:0,  //已回答问题个数
-    questions:[
-      "",  //仅用来占位，nowIndex从1开始
+    currentChosen: -1,
+    nowIndex: 1,
+    chosenAnswers: [0], //第0位仅用来占位，真正的index从1开始
+    chosenAnswersLength: 0, //已回答问题个数
+    questions: [
+      "", //仅用来占位，nowIndex从1开始
       "反复想起过去紧张的经历，苦恼不安，想忘却忘不掉？",
       "过往紧张的经历反复入梦，苦恼不安？",
       "突然感觉或表现得好似过往不好的经历又实实在在地发生了（以为您又真切地回到过去一样）？",
@@ -44,8 +48,8 @@ Page({
       "“过于戒备”、警惕或提防？",
       "提心吊胆或易受惊吓？",
       "注意力难以集中？",
-      "入睡困难或睡不安稳？"
-    ]
+      "入睡困难或睡不安稳？",
+    ],
   },
 
   //选择答案
@@ -55,37 +59,39 @@ Page({
     let nowIndex = this.data.nowIndex;
     let chosenAnswers = this.data.chosenAnswers;
     chosenAnswers[nowIndex] = currentChosen;
-    if(nowIndex===chosenAnswersLength+1) chosenAnswersLength++;
+    if (nowIndex === chosenAnswersLength + 1) chosenAnswersLength++;
     this.setData({
-      currentChosen:currentChosen,
-      chosenAnswers:chosenAnswers,
-      chosenAnswersLength:chosenAnswersLength
-    })
+      currentChosen: currentChosen,
+      chosenAnswers: chosenAnswers,
+      chosenAnswersLength: chosenAnswersLength,
+    });
   },
 
   //点击回到上一题
-  tapPrev(){
-    let prevIndex = this.data.nowIndex-1;
+  tapPrev() {
+    let prevIndex = this.data.nowIndex - 1;
     let prevChoosen = this.data.chosenAnswers[prevIndex];
     this.setData({
-      nowIndex:prevIndex,
-      currentChosen:prevChoosen
-    })
+      nowIndex: prevIndex,
+      currentChosen: prevChoosen,
+    });
   },
 
   //点击下一题 or 提交--> 根据nowIndex判断是下一题还是查看结果分析
   tapNext() {
-    if(this.data.currentChosen===-1){   //未选择答案，驳回
+    if (this.data.currentChosen === -1) {
+      //未选择答案，驳回
       wx.showToast({
-        title: '请选择一项',
-        icon:'none'
-      })
-    }else{
-      if(this.data.nowIndex===20){     //当前index为最后一个题目，准备跳转到结果分析页面
+        title: "请选择一项",
+        icon: "none",
+      });
+    } else {
+      if (this.data.nowIndex === 20) {
+        //当前index为最后一个题目，准备跳转到结果分析页面
         this.calculate();
         this.setData({
-          showCalculation:true
-        })
+          showCalculation: true,
+        });
         // ↓ ********* 保存分析结果到数据库 ********* ↓
         let testName = "创伤后应激障碍筛查量表PCL-5";
         let totalScores = this.data.totalScores;
@@ -93,71 +99,85 @@ Page({
         let advice = this.data.final_eval_str;
         let date = Date.now();
         let totalColor = this.data.totalColor;
-        for(var i=0;i<this.data.Parts_Scores.length;i++){
-          partScores.push(String(this.data.Parts_Scores[i][0]+"："+this.data.Parts_Scores[i][1]+"分（"+this.data.Parts_Scores[i][2]+"）；"))
+        for (var i = 0; i < this.data.Parts_Scores.length; i++) {
+          partScores.push(
+            String(
+              this.data.Parts_Scores[i][0] +
+                "：" +
+                this.data.Parts_Scores[i][1] +
+                "分（" +
+                this.data.Parts_Scores[i][2] +
+                "）；"
+            )
+          );
         }
-        db.collection("index1_adviceResult").add({
-          data:{
-            testName:testName,
-            totalScores:totalScores,
-            partScores:partScores,
-            advice:advice,
-            date:date,
-            totalColor:totalColor,
-          }
-        }).then(res=>{
-          console.log("测评结果添加入数据库")
-        })
+        db.collection("index1_adviceResult")
+          .add({
+            data: {
+              testName: testName,
+              totalScores: totalScores,
+              partScores: partScores,
+              advice: advice,
+              date: date,
+              totalColor: totalColor,
+            },
+          })
+          .then((res) => {
+            console.log("测评结果添加入数据库");
+          });
 
         // ↑ ********* 保存分析结果到数据库 ********* ↑
-
-      }else{                           //当前index为中间题目,继续在本页面渲染
+      } else {
+        //当前index为中间题目,继续在本页面渲染
         //从未问答过下一题
-        if(this.data.nowIndex>=this.data.chosenAnswersLength){
+        if (this.data.nowIndex >= this.data.chosenAnswersLength) {
           let nowIndex = this.data.nowIndex;
           this.setData({
-            currentChosen:-1,
-            nowIndex:nowIndex+1,
-          })
-        }else{
+            currentChosen: -1,
+            nowIndex: nowIndex + 1,
+          });
+        } else {
           // //已经回答过下一题
           let nowIndex = this.data.nowIndex;
-          let nextIndex = nowIndex+1;
+          let nextIndex = nowIndex + 1;
           let nextChosen = this.data.chosenAnswers[nextIndex];
           this.setData({
-            currentChosen:nextChosen,
-            nowIndex:nextIndex,
-          })
+            currentChosen: nextChosen,
+            nowIndex: nextIndex,
+          });
         }
       }
     }
   },
 
   //计算得分
-  calculate(){
+  calculate() {
     let totalScores = 0;
-    let Parts_Scores_tmp = new Array(this.data.Parts_numbers.length).fill(0);    //根据各部分总数创建各部分的scores数组
+    let Parts_Scores_tmp = new Array(this.data.Parts_numbers.length).fill(0); //根据各部分总数创建各部分的scores数组
     let final_eval_str = "";
     let totalColor = "";
-    for(var i=1;i<=20;i++){                     //计算各部分得分
-      for(var j=0;j<4;j++){
-        if(this.data.Parts_numbers[j].indexOf(i)>=0){
+    for (var i = 1; i <= 20; i++) {
+      //计算各部分得分
+      for (var j = 0; j < 4; j++) {
+        if (this.data.Parts_numbers[j].indexOf(i) >= 0) {
           Parts_Scores_tmp[j] += this.data.chosenAnswers[i];
           break;
         }
       }
       totalScores += this.data.chosenAnswers[i];
     }
-    if(totalScores==0){
+    if (totalScores == 0) {
       final_eval_str = "恭喜，本次测评未发现PTSD症状！";
       totalColor = "green";
-    }else if(totalScores < 19){
-      final_eval_str = "有极轻微PTSD症状，可自行观察一段时间，或向心理医师寻求建议。";
+    } else if (totalScores < 19) {
+      final_eval_str =
+        "有极轻微PTSD症状，可自行观察一段时间，或向心理医师寻求建议。";
       totalColor = "blue";
-    }else if(totalScores < 38){
-      final_eval_str = "有轻微PTSD症状，可自行观察一段时间，或向心理医师寻求建议。";
+    } else if (totalScores < 38) {
+      final_eval_str =
+        "有轻微PTSD症状，可自行观察一段时间，或向心理医师寻求建议。";
       totalColor = "orange";
-    }else{
+    } else {
       final_eval_str = "有较严重PTSD症状，请立即向心理咨询师寻求帮助！";
       totalColor = "red";
     }
@@ -166,37 +186,40 @@ Page({
     let Parts_Eva_tmp = [];
     let tmpstr = "";
     let Parts_meanings = this.data.Parts_meanings;
-    for(var i=0;i<this.data.Parts_numbers.length;i++){
-      if(Parts_Scores_tmp[i]/this.data.Parts_numbers[i].length<1){
+    for (var i = 0; i < this.data.Parts_numbers.length; i++) {
+      if (Parts_Scores_tmp[i] / this.data.Parts_numbers[i].length < 1) {
         tmpstr = "无明显症状";
-      }else if (Parts_Scores_tmp[i]/this.data.Parts_numbers[i].length<2){
+      } else if (Parts_Scores_tmp[i] / this.data.Parts_numbers[i].length < 2) {
         tmpstr = "症状轻微";
-      }else if (Parts_Scores_tmp[i]/this.data.Parts_numbers[i].length<3){
+      } else if (Parts_Scores_tmp[i] / this.data.Parts_numbers[i].length < 3) {
         tmpstr = "中等症状";
-      }else if (Parts_Scores_tmp[i]/this.data.Parts_numbers[i].length<4){
+      } else if (Parts_Scores_tmp[i] / this.data.Parts_numbers[i].length < 4) {
         tmpstr = "症状较严重";
-      }else{
+      } else {
         tmpstr = "症状严重";
       }
       Parts_Eva_tmp.push(tmpstr);
     }
     let Parts_Scores = [];
-    for(var i=0;i<this.data.Parts_numbers.length;i++){
-      Parts_Scores.push([Parts_meanings[i],Parts_Scores_tmp[i],Parts_Eva_tmp[i]])
+    for (var i = 0; i < this.data.Parts_numbers.length; i++) {
+      Parts_Scores.push([
+        Parts_meanings[i],
+        Parts_Scores_tmp[i],
+        Parts_Eva_tmp[i],
+      ]);
     }
 
     this.setData({
       Parts_Scores,
       totalScores,
       final_eval_str,
-      totalColor
-    })
+      totalColor,
+    });
   },
 
-  navback(){
+  navback() {
     wx.navigateBack({
       delta: 2,
-    })
+    });
   },
-
-})
+});
--- miniprogram/pages/index2/index2.js
+++ miniprogram/pages/index2/index2.js
@@ -1,19 +1,30 @@
 // pages/index2/index2.js
-const app = getApp()
-const date = new Date()
-const db = wx.cloud.database()
-const monthList = ["1","2","3","4","5","6","7","8","9","10","11","12"]
-const years = []
-const dates = []
+const app = getApp();
+const date = new Date();
+const db = wx.cloud.database();
+const monthList = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+];
+const years = [];
+const dates = [];
 for (let i = 2000; i <= date.getFullYear(); i++) {
-  years.push(i)
+  years.push(i);
 }
 for (let i = 1; i <= date.getDate(); i++) {
-  dates.push(i)
+  dates.push(i);
 }
 Page({
-  
-
   /**
    * 页面的初始数据
    */
@@ -25,15 +36,15 @@ Page({
       emotion: "/pages/index2/treeHole/3/emotion",
       study: "/pages/index2/treeHole/4/study",
       game: "/pages/index2/treeHole/5/game",
-      exam: "/pages/index2/treeHole/6/exam"
+      exam: "/pages/index2/treeHole/6/exam",
     },
     // 各个子树洞的图片
     work: "/pages/index2/logo/work.svg",
     game: "/pages/index2/logo/game.svg",
-    emotion:"/pages/index2/logo/emotion.svg",
-    exam:"/pages/index2/logo/exam.svg",
+    emotion: "/pages/index2/logo/emotion.svg",
+    exam: "/pages/index2/logo/exam.svg",
     life: "/pages/index2/logo/life.svg",
-    study:"/pages/index2/logo/study.svg",
+    study: "/pages/index2/logo/study.svg",
     // 轮播图
     picList: [
       "cloud://tangerine-cloud-9grdz5e80159e7b3.7461-tangerine-cloud-9grdz5e80159e7b3-1304921980/index2/slides/slide0.jpg",
@@ -41,175 +52,186 @@ Page({
       "cloud://tangerine-cloud-9grdz5e80159e7b3.7461-tangerine-cloud-9grdz5e80159e7b3-1304921980/index2/slides/slide2.jpg",
       "cloud://tangerine-cloud-9grdz5e80159e7b3.7461-tangerine-cloud-9grdz5e80159e7b3-1304921980/index2/slides/slide3.jpg",
     ],
-    commentList:{//每个月的评论
-      "1":"",
-      "2":"",
-      "3":"",
-      "4":"",
-      "5":"",
-      "6":"",
-      "7":"",
-      "8":"",
-      "9":"",
-      "10":"",
-      "11":"",
-      "12":""
+    commentList: {
+      //每个月的评论
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: "",
+      6: "",
+      7: "",
+      8: "",
+      9: "",
+      10: "",
+      11: "",
+      12: "",
     },
     tempTreeholeName: null, // 临时存放树洞名称的地方
-    cur_question:"",
+    cur_question: "",
     cur_date: date.getDate().toString(),
     cur_year: date.getFullYear().toString(),
     valueYear: [9999],
     valueDate: [9999],
-    years,dates
+    years,
+    dates,
   },
 
   /**
    * 跳转到对应分区
    */
-  gotoTreehole: function(e) {
-    let temp = e.currentTarget.dataset.treeholename
+  gotoTreehole: function (e) {
+    let temp = e.currentTarget.dataset.treeholename;
     // console.log("即将前往树洞：",temp, this.data.TreeholeAddress[temp])
     // let tempurl = this.data.TreeholeAddress + "?title=" + temp
-    let tempurl = "/pages/index2/treeHole/treeHole?title=" + temp
+    let tempurl = "/pages/index2/treeHole/treeHole?title=" + temp;
     wx.navigateTo({
-      url: tempurl
-    })
+      url: tempurl,
+    });
   },
 
   /**
    * 跳转到树洞编辑页面
    */
-  gotoPersonalEditor: function() {
-    if (app.globalData.isLogin){
+  gotoPersonalEditor: function () {
+    if (app.globalData.isLogin) {
       wx.navigateTo({
-        url: "/pages/index2/editPage/personalEditor"
-      })
+        url: "/pages/index2/editPage/personalEditor",
+      });
     }
     // 如果没有登录则提醒先登录
     else {
       wx.switchTab({
-        url: '/pages/index3/index3',
-      })
+        url: "/pages/index3/index3",
+      });
       wx.showToast({
-        title: '请先登录',
-        icon: 'none',
-        duration: 1500
-      })
+        title: "请先登录",
+        icon: "none",
+        duration: 1500,
+      });
     }
   },
-  bindYearPickerChange: function(e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+  bindYearPickerChange: function (e) {
+    console.log("picker发送选择改变，携带值为", e.detail.value);
     this.setData({
-      index_year: e.detail.value
-    })
+      index_year: e.detail.value,
+    });
   },
   // 年份选择的响应事件
-  bindChangeYear: function(e) {
-    const val1 = e.detail.value
+  bindChangeYear: function (e) {
+    const val1 = e.detail.value;
     this.setData({
       cur_year: this.data.years[val1[0]].toString(),
-    })
+    });
     // this.getQuestion() 由于目前每年的题目一样，所以这里在更新年份后，可以不用跟着更新问题
-    monthList.forEach((month)=>
-     this.getCommentPic(month)
-    );
+    monthList.forEach((month) => this.getCommentPic(month));
   },
   // 日期选择的响应事件
-  bindChangeDate: function(e) {
-    const val2 = e.detail.value
+  bindChangeDate: function (e) {
+    const val2 = e.detail.value;
     this.setData({
       cur_date: this.data.dates[val2[0]].toString(),
-    })
-    this.getQuestion()
-    monthList.forEach((month)=>
-     this.getCommentPic(month)
-    );
+    });
+    this.getQuestion();
+    monthList.forEach((month) => this.getCommentPic(month));
   },
-  goToheartRec: function(e) {
-    let tempurl = "/pages/index2/heartRec/heartRec?date=" + this.data.cur_year+"-"+this.data.cur_date;
+  goToheartRec: function (e) {
+    let tempurl =
+      "/pages/index2/heartRec/heartRec?date=" +
+      this.data.cur_year +
+      "-" +
+      this.data.cur_date;
     wx.navigateTo({
-      url: tempurl
-    })
-    wx.navigateTo({
-      url: "/pages/index2/heartRec/heartRec"
-    })
+      url: tempurl,
+    });
+    // wx.navigateTo({
+    //   url: "/pages/index2/heartRec/heartRec"
+    // })
   },
 
-  onLoad(){
+  onLoad() {
     // let date = new Date();
     // console.log(date.getDate())
     // this.setData({
     //   cur_date : date.getDate().toString()
     // })
-    console.log('是否登录',app.globalData.isLogin,date)
+    console.log("是否登录", app.globalData.isLogin, date);
   },
 
-  getQuestion:function(){
-    const that = this
-    db.collection('index2HeartRec_question').where({
-      day: that.data.cur_date
-    }).field({
-      question: true
-    }).limit(100) // 限制返回结果数量
-    .get({
-      success: function(res){
-        console.log("问题查询成功", res)
-        that.setData({
-          cur_question: res.data[0].question
-        })
-      },
-      fail: function(err) {
-        console.log('查询失败', err);
-      }
-    });
+  getQuestion: function () {
+    const that = this;
+    db.collection("index2HeartRec_question")
+      .where({
+        day: that.data.cur_date,
+      })
+      .field({
+        question: true,
+      })
+      .limit(100) // 限制返回结果数量
+      .get({
+        success: function (res) {
+          console.log("问题查询成功", res);
+          that.setData({
+            cur_question: res.data[0].question,
+          });
+        },
+        fail: function (err) {
+          console.log("查询失败", err);
+        },
+      });
   },
-   // 获取评论
-   getCommentPic:function(month){
-    const that = this
-    let openid = app.globalData.openid
-    db.collection('index2HeartRec_commentAndpicture').where({
-      _openid: openid,
-      year: this.data.cur_year,
-      month: month,
-      day: this.data.cur_date
-    }).field({
-      // picID: true,
-      comment: true,
-      isNone: true
-    }).limit(100) // 限制返回结果数量
-    .get({
-      success: function(res) {
-        console.log('查询成功', month,res.data.length);
-       // 获取评论
-       let monthKey = "commentList[" + month + "]"
-      //  let imageKey = "imageList[" + month + "]"
-       if(res.data.length==0){//当未查询到数据时，显示默认评论
-        that.setData({
-        [monthKey]:"等待你来探索..."
-        // [imageKey]: [defaultImg]
-        })
-       }
-       that.setData({//当未查询到数据时，这段代码不会执行
-         [monthKey]: (res.data[0].comment.length==0) ? "等待你来探索...":res.data[0].comment
-       })
-       // 获取图片
-      //  let fileIDs = res.data.map(item => item.picID); // 将 fileID 提取出来
-      //  console.log('fileIDs', fileIDs); // 打印包含所有 picID 的数组
-      //  let imageKey = "imageList[" + month + "]"
-      //  that.setData({//当未查询到数据时，这段代码不会执行
-      //    [imageKey]: fileIDs
-      //  })
-      },
-      fail: function(err) {
-        console.error('查询失败', err);
-      }
-    });
+  // 获取评论
+  getCommentPic: function (month) {
+    const that = this;
+    let openid = app.globalData.openid;
+    db.collection("index2HeartRec_commentAndpicture")
+      .where({
+        _openid: openid,
+        year: this.data.cur_year,
+        month: month,
+        day: this.data.cur_date,
+      })
+      .field({
+        // picID: true,
+        comment: true,
+        isNone: true,
+      })
+      .limit(100) // 限制返回结果数量
+      .get({
+        success: function (res) {
+          console.log("查询成功", month, res.data.length);
+          // 获取评论
+          let monthKey = "commentList[" + month + "]";
+          //  let imageKey = "imageList[" + month + "]"
+          if (res.data.length == 0) {
+            //当未查询到数据时，显示默认评论
+            that.setData({
+              [monthKey]: "等待你来探索...",
+              // [imageKey]: [defaultImg]
+            });
+          }
+          that.setData({
+            //当未查询到数据时，这段代码不会执行
+            [monthKey]:
+              res.data[0].comment.length == 0
+                ? "等待你来探索..."
+                : res.data[0].comment,
+          });
+          // 获取图片
+          //  let fileIDs = res.data.map(item => item.picID); // 将 fileID 提取出来
+          //  console.log('fileIDs', fileIDs); // 打印包含所有 picID 的数组
+          //  let imageKey = "imageList[" + month + "]"
+          //  that.setData({//当未查询到数据时，这段代码不会执行
+          //    [imageKey]: fileIDs
+          //  })
+        },
+        fail: function (err) {
+          console.error("查询失败", err);
+        },
+      });
   },
   onShow() {
-    this.getQuestion()
-    monthList.forEach((month)=>
-     this.getCommentPic(month)
-    );
+    this.getQuestion();
+    monthList.forEach((month) => this.getCommentPic(month));
   },
-})
+});
